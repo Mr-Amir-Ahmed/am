814c0bbce9279f129bd259004ff417fdc246bd91
Mr-Amir-Ahmed <110778806+Mr-Amir-Ahmed@users.noreply.github.com>
2024-05-28 05:14:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.typing-name', {
+    strings: [' Amir Ahmed '],
+    typeSpeed:280,
+});
+var typed = new Typed('.typing-text', {
+    strings: [' programer','network adminstrator','Information Technology Engineer','Project Management','web developer','Management Information Systems','UX/UI Desiner','data entry'],
+    typeSpeed:115,
+    loop: 1,
+});
+var typed = new Typed('.typing-paragraph', {
+    strings: [' a graduate of El Shorouk Academy, majoring in Management Information Systems. I have dealt with many programming languages for websites and applications, I got an A in my graduation project with flutter programming language, now I am a network and server specialist and aspire to enter the field of information security, I hope to be accepted in this job so that I can get practical experience in the field of networks or information technology, I am looking for more scientific and informational experiences and I hope to join your group.'],
+    typeSpeed:7,
+    
+});
+
